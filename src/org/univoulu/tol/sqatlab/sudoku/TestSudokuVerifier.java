0bae02643986b692a340d05ed4f6ce7cdd9d69ae
somename <someemail>
2015-10-02 13:19:23 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -10,7 +10,7 @@ public class TestSudokuVerifier {
 	public void test() {
 		SudokuVerifier sv = new SudokuVerifier();
 		
-		String candidateSolution = "4173698256321583947958724316825437169791586432346912758289643571573291684164875293" ;
+		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293" ;
 		sv.verify(candidateSolution);
 		
 	}
