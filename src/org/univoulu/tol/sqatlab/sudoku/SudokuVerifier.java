4140b4d3fa668ab86f48c91c0810cf1d268db293
somename <someemail>
2015-10-02 12:14:19 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -6,18 +6,28 @@ public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
 		
+//R1: A cell in a Sudoku game can only store positive digits, i.e. 1...9.
+//R2: All digits appear only once in a sub-grid, i.e. they cannot repeat.
+//R3: A digit can appear only once in the rows of the global grid.
+//R4: A digit can appear only once in the columns of the global grid.
+		
 		int length = candidateSolution.length();
+		boolean enoughNumbers;
 		
 		
 		if(length == 81) {
 			
 			System.out.print("Enough characters");
-			
+			enoughNumbers = true;
 		}
 		
 		else {
 			System.out.print("No 81 numbers");
+			enoughNumbers = false;
 		}
+		
+		if()
+		
 		
 		// returns 1 if the candidate solution is correct
 		return 0;
