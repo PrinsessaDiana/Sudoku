62b1c244ecccf4aad5e845cd5ff1527c0a5b7e62
somename <someemail>
2015-10-02 13:25:53 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -12,6 +12,7 @@ public class TestSudokuVerifier {
 		
 		String candidateSolution = "417369825632158947958724316825437169791586432346912758289643571573291684164875293" ;
 		sv.verify(candidateSolution);
+		assertEquals(1,1);
 		
 	}
 
